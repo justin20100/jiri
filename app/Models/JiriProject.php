0916e470d549d51fdbf855498619901db3a4c5e2
justinvincent <justin.vincent@student.hepl.be>
2024-01-04 15:42:31 +0100

    writing JiriProject model

--- app/Models/JiriProject.php
+++ app/Models/JiriProject.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class JiriProject extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'jiri_id',
+    ];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function jiri()
+    {
+        return $this->belongsTo(Jiri::class);
+    }
+
 }
---
+++ database/factories/JiriProjectFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\JiriProject;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class JiriProjectFactory extends Factory
+{
+    protected $model = JiriProject::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
