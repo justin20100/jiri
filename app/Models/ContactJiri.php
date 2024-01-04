0b0114474ed43f27fee72cfe21d8b9f6b9655d17
justinvincent <justin.vincent@student.hepl.be>
2024-01-04 15:41:02 +0100

    writing ContactJiri model

--- app/Models/ContactJiri.php
+++ app/Models/ContactJiri.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ContactJiri extends Model
 {
     use HasFactory;
+
+    protected  $fillable = [
+        'role',
+        'token',
+        'contact_id',
+        'jiri_id',
+    ];
+
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
+
+    public function jiri(): BelongsTo
+    {
+        return $this->belongsTo(Jiri::class);
+    }
 }
---
+++ database/factories/ContactJiriFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ContactJiri;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ContactJiriFactory extends Factory
+{
+    protected $model = ContactJiri::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}
