1ff9b262ae2436a2bf2be0357600d082618738e6
justinvincent <justin.vincent@student.hepl.be>
2024-01-04 14:26:45 +0100

    writting Jiri model

--- app/Models/Jiri.php
+++ app/Models/Jiri.php
@@ -4,8 +4,42 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Jiri extends Model
 {
     use HasFactory;
+
+    protected $fillable= [
+        'name',
+        'start',
+        'end',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function contactDuties(): HasMany
+    {
+        return $this->hasMany(ContactDuty::class);
+    }
+
+    public function projects(): BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'jiri_project', 'jiri_id', 'project_id');
+    }
+
+    public function contactsJiris(): HasMany
+    {
+        return $this->hasMany(ContactJiri::class);
+    }
+
+    public function contacts(): BelongsToMany
+    {
+        return $this->belongsToMany(Contact::class, 'contact_jiri', 'jiri_id', 'contact_id');
+    }
 }
