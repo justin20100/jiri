7fc88ff61a958a8738187333e65b76ce41f73e94
justinvincent <justin.vincent@student.hepl.be>
2024-01-09 22:27:06 +0100

    styling welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,79 +36,101 @@
                     <div class="wrapper">
                         <div class="banner__contentContainer">
                             <div class="banner__contentContainer__content">
-                                <h1 class="banner__ContentContainer__content__title">Simplifiez, digitalisez et  centralisez vos données lors de jurys</h1>
-                                <p class="banner__ContentContainer__content__text">Créez des contacts ainsi que des projets pour en faire un Jiri. Ce Jiri une fois crée, donne aux jurys de celui-ci accès à une interface pour noter les étudiants sur un ou plusieurs projets.</p>
-                                <div class="banner__ContentContainer__content__buttonContainer">
-                                    <a href="{{route('register')}}" class="banner__ContentContainer__content__buttonContainer__button">Créer un compte</a>
-                                    <a href="{{route('login')}}" class="banner__ContentContainer__content__buttonContainer__button">En savoir plus</a>
+                                <h1 class="banner__contentContainer__content__title">Simplifiez, digitalisez et  centralisez vos données lors de jurys</h1>
+                                <p class="banner__contentContainer__content__text">Créez des contacts ainsi que des projets pour en faire un Jiri. Ce Jiri une fois crée, donne aux jurys de celui-ci accès à une interface pour noter les étudiants sur un ou plusieurs projets.</p>
+                                <div class="banner__contentContainer__content__buttonContainer">
+                                    <a href="#features" class="banner__contentContainer__content__buttonContainer__button button button--light">En savoir plus</a>
+                                    <a href="{{route('register')}}" class="banner__contentContainer__content__buttonContainer__button button">Créer un compte</a>
                                 </div>
                             </div>
-                            <div class="banner__contentContainer__image">
-                                <img src="{{asset('/images/welcome/welcome-screen-capture.jpg')}}" alt="" class="banner__ContentContainer__image__img">
+                            <div class="banner__contentContainer__screen">
+                                    <img src="{{asset('/images/welcome/welcome-screen-capture.jpg')}}" alt="" class="banner__contentContainer__screen__img">
                             </div>
-                            <img src="{{asset('/images/welcome/welcome-first-illu.jpg')}}" alt="" class="banner__contentContainer__Illu--first">
-                            <img src="{{asset('/images/welcome/welcome-second-illu.jpg')}}" alt="" class="banner__contentContainer__Illu--second">
+                            <img src="{{asset('/images/welcome/welcome-first-illu.jpg')}}" alt="" class="banner__contentContainer__illu banner__contentContainer__illu--first">
+                            <img src="{{asset('/images/welcome/welcome-second-illu.jpg')}}" alt="" class="banner__contentContainer__illu banner__contentContainer__illu--second">
                         </div>
                     </div>
                 </section>
-                <section class="features">
+                <section class="features" id="features">
                     <div class="wrapper">
                         <div class="features__contentContainer">
                             <h2 class="features__contentContainer__title">{{__('Fontionnalités')}}</h2>
                             <div class="features__contentContainer__list">
 
                                 <div class="features__contentContainer__list__item">
-                                    <div class="features__contentContainer__list__item__iconContainer">
-                                        <svg class="features__contentContainer__list__item__iconContainer__icon">
-                                            <use xlink:href="#lala"></use>
-                                        </svg>
+                                    <div class="features__contentContainer__list__item__top">
+                                        <div class="features__contentContainer__list__item__top__iconContainer features__contentContainer__list__item__top__iconContainer--1">
+                                            <svg class="features__contentContainer__list__item__top__iconContainer__icon">
+                                                <use xlink:href="{{asset("images/sprite.svg#feature1")}}"></use>
+                                            </svg>
+                                        </div>
+                                        <h3 class="features__contentContainer__list__item__top__title">Simplification de l’encodage des cotes par le jury</h3>
                                     </div>
-                                    <h3 class="features__contentContainer__list__item__title">Simplification de l’encodage des cotes par le jury</h3>
                                     <p class="features__contentContainer__list__item__text">
                                         Plus besoin de feuille ou de tableau Excel ou les fautes d’encodages sont vite arrivées. Une interface web simple est a disposition du jury.
                                     </p>
                                 </div>
 
                                 <div class="features__contentContainer__list__item">
-                                    <div class="features__contentContainer__list__item__iconContainer">
-                                        <svg class="features__contentContainer__list__item__iconContainer__icon">
-                                            <use xlink:href="#lala"></use>
-                                        </svg>
+                                    <div class="features__contentContainer__list__item__top">
+                                        <div class="features__contentContainer__list__item__top__iconContainer features__contentContainer__list__item__top__iconContainer--2">
+                                            <svg class="features__contentContainer__list__item__top__iconContainer__icon">
+                                                <use xlink:href="{{asset("images/sprite.svg#feature2")}}"></use>
+                                            </svg>
+                                        </div>
+                                        <h3 class="features__contentContainer__list__item__top__title">Centralisation de vos jurys sur cette application</h3>
                                     </div>
-                                    <h3 class="features__contentContainer__list__item__title">Centralisation de vos jurys sur cette application</h3>
                                     <p class="features__contentContainer__list__item__text">
                                         Gardez un historique des jurys, faite grandir votre liste de contacts, certains étudiants deviendront peut-être jury
                                     </p>
                                 </div>
 
                                 <div class="features__contentContainer__list__item">
-                                    <div class="features__contentContainer__list__item__iconContainer">
-                                        <svg class="features__contentContainer__list__item__iconContainer__icon">
-                                            <use xlink:href="#lala"></use>
-                                        </svg>
+                                    <div class="features__contentContainer__list__item__top">
+                                        <div class="features__contentContainer__list__item__top__iconContainer features__contentContainer__list__item__top__iconContainer--3">
+                                            <svg class="features__contentContainer__list__item__top__iconContainer__icon">
+                                                <use xlink:href="{{asset("images/sprite.svg#feature3")}}"></use>
+                                            </svg>
+                                        </div>
+                                        <h3 class="features__contentContainer__list__item__top__title">Exportation de vos tableaux de cotes une fois clôturés</h3>
                                     </div>
-                                    <h3 class="features__contentContainer__list__item__title">Exportation de vos tableaux de cotes une fois clôturés</h3>
                                     <p class="features__contentContainer__list__item__text">
                                         Plus besoin d’encoder toutes les cotes, exportez directement vos tableaux
                                     </p>
                                 </div>
 
                                 <div class="features__contentContainer__list__item">
-                                    <div class="features__contentContainer__list__item__iconContainer">
-                                        <svg class="features__contentContainer__list__item__iconContainer__icon">
-                                            <use xlink:href="#lala"></use>
-                                        </svg>
+                                    <div class="features__contentContainer__list__item__top">
+                                        <div class="features__contentContainer__list__item__top__iconContainer features__contentContainer__list__item__top__iconContainer--4">
+                                            <svg class="features__contentContainer__list__item__top__iconContainer__icon">
+                                                <use xlink:href="{{asset("images/sprite.svg#featur4")}}"></use>
+                                            </svg>
+                                        </div>
+                                        <h3 class="features__contentContainer__list__item__top__title">Invitation automatique des membres du jury</h3>
                                     </div>
-                                    <h3 class="features__contentContainer__list__item__title">Invitation automatique des membres du jury</h3>
                                     <p class="features__contentContainer__list__item__text">
                                         Chaque membre du jury recois un mail pour se connecter a l’interface automatiquement
                                     </p>
                                 </div>
-
                             </div>
                         </div>
                     </div>
                 </section>
+                <footer class="footer">
+                    <div class="wrapper">
+                        <div class="footer__contentContainer">
+                            <div class="footer__contentContainer__links">
+                                <div class="footer__contentContainer__links__buttonsContainer">
+                                    <a href="{{route('login')}}" class="footer__contentContainer__links__buttonsContainer__button button button--white">Se connecter</a>
+                                    <a href="{{route('register')}}" class="footer__contentContainer__links__buttonsContainer__button button">Créer un compte</a>
+                                </div>
+                            </div>
+                            <div class="footer__contentContainer__legal">
+                                <a href="#" class="footer__contentContainer__legal__link">Mentions légales</a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </main>
         </div>
     </body>
