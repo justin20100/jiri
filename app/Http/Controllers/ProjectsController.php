7876edde18a3a1ee3ce56ddb5a10e242ce50edfc
justinvincent <justin.vincent@student.hepl.be>
2024-01-10 15:10:54 +0100

    starting styling aside navigation

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
@@ -13,7 +14,7 @@ class ProjectsController extends Controller
     {
         $user = auth()->user();
 
-        $projects = $user->projects()->get();
+        $projects = Project::orderBy('created_at', 'desc')->where('user_id', $user->id);
 
         return view('projects.index', compact('projects'));
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,24 +8,26 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+{{--        <link rel="preconnect" href="https://fonts.bunny.net">--}}
+{{--        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />--}}
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="appBody">
+        <div class="appBody__contentContainer">
             <livewire:layout.navigation />
 
+
+
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+{{--            @if (isset($header))--}}
+{{--                <header class="">--}}
+{{--                    <div class="">--}}
+{{--                        {{ $header }}--}}
+{{--                    </div>--}}
+{{--                </header>--}}
+{{--            @endif--}}
 
             <!-- Page Content -->
             <main>
