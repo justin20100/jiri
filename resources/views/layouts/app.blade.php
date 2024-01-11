a613922bed274623a91265f8aa5625101672bd7a
justinvincent <justin.vincent@student.hepl.be>
2024-01-11 15:37:50 +0100

    adding appbar to app layout with search and profile link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,18 +7,12 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-{{--        <link rel="preconnect" href="https://fonts.bunny.net">--}}
-{{--        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />--}}
-
         <!-- Scripts -->
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body class="appBody">
         <div class="appBody__contentContainer">
             <livewire:layout.navigation />
-
-
 
             <!-- Page Heading -->
 {{--            @if (isset($header))--}}
@@ -30,7 +24,30 @@
 {{--            @endif--}}
 
             <!-- Page Content -->
-            <main>
+            <main class="appBody__contentContainer__main">
+                @php($currentUser = Auth()->user())
+                <div class="appBar">
+                    <div class="appBar__contentContainer">
+                        <div class="appBar__contentContainer__searchContainer">
+                            <div class="appBar__contentContainer__searchContainer__iconContainer">
+                                <svg>
+                                    <use xlink:href="{{asset("images/sprite.svg#search")}}"></use>
+                                </svg>
+                            </div>
+                            <input type="text" class="appBar__contentContainer__searchContainer__input" placeholder="{{__('Votre recherche ...')}}">
+                        </div>
+
+                        <x-dropdown-link class="appBar__contentContainer__profileContainer" :href="route('profile')" wire:navigate>
+                            <div class="appBar__contentContainer__profileContainer__nameContainer">
+                                <span class="appBar__contentContainer__profileContainer__nameContainer__name">{{'Bonjour, '.$currentUser['firstname']}}</span>
+                            </div>
+                            <div class="appBar__contentContainer__profileContainer__imgContainer">
+                                <img src="{{URL::to('/storage/avatars')."/".$currentUser['avatar']}}" alt="" class="appBar__contentContainer__profileContainer__imgContainer__img">
+                            </div>
+                        </x-dropdown-link>
+                    </div>
+                </div>
+
                 {{ $slot }}
             </main>
         </div>
