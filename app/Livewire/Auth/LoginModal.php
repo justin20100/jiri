2df573d2332e5125eee24f4ac3cb3fef823671b0
justinvincent <justin.vincent@student.hepl.be>
2024-07-17 14:46:17 +0200

    Adding livewire component LoginModal

---
+++ app/Livewire/Auth/LoginModal.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Livewire\Auth;
+
+use Livewire\Component;
+use App\Livewire\Forms\LoginForm;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Session;
+
+class LoginModal extends Component
+{
+    public LoginForm $form;
+
+    public function mount(): void
+    {
+        $this->form = new LoginForm();
+    }
+
+    public function login(): void
+    {
+        $this->validate();
+
+        $this->form->authenticate();
+
+        Session::regenerate();
+
+        $this->redirect(
+            session('url.intended', RouteServiceProvider::HOME),
+            navigate: true
+        );
+    }
+
+    public function render()
+    {
+        return view('livewire.auth.login-modal');
+    }
+}
