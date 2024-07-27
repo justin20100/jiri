63066e4bf52d00becd6d98a0f2fb518bc5fb8c7d
justinvincent <justin.vincent@student.hepl.be>
2024-07-27 11:28:31 +0200

    adding WelcomeModal component

---
+++ app/Livewire/Welcome/WelcomeModal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Welcome;
+
+use Livewire\Component;
+
+class WelcomeModal extends Component
+{
+    public function render()
+    {
+        return view('livewire.welcome.welcome-modal');
+    }
+}
