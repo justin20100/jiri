825dde6455bdecc0a03d2c99065871459ed5df47
justinvincent <justin.vincent@student.hepl.be>
2024-08-02 10:26:45 +0200

    Update Jiricreate

--- app/Livewire/Jiris/JiriCreate.php
+++ app/Livewire/Jiris/JiriCreate.php
@@ -10,13 +10,21 @@ class JiriCreate extends Component
 {
     public $projects;
     public $contacts;
-//    protected $listeners = [
-//        'refreshProjectList' => 'refreshProjectList',
-//        'refreshContactList' => 'refreshContactList',
-//    ];
-//    public $selectedProjects;
-//    public $selectedContacts;
+    protected $listeners = [
+        'refreshProjectList' => 'refreshProjectList',
+        'refreshContactList' => 'refreshContactList',
+    ];
+    public $selectedProjects;
+    public $selectedContacts;
     public bool $actionsDisabled = true;
+
+    public function mount(): void
+    {
+        $this->projects = Auth::user()->projects()->orderby('created_at', 'desc')->get();
+        $this->contacts = Auth::user()->contacts()->orderby('created_at', 'desc')->get();
+        $this->selectedProjects = collect();
+        $this->selectedContacts = collect();
+    }
 
     #[Computed]
     public function projects(): array
@@ -39,69 +47,62 @@ class JiriCreate extends Component
 
     }
 
-//    public function mount(): void
-//    {
-//        $this->projects = collect();
-//        $this->contacts = collect();
-//        $this->selectedProjects = collect();
-//        $this->selectedContacts = collect();
-//    }
 
     public function render()
     {
         return view('livewire.jiris.jiri-create');
     }
 
-//    public function refreshProjectList(): void
-//    {
-//        $this->projects = Auth::user()->projects()->orderBy('created_at', 'desc')->get();
-//    }
+    public function refreshProjectList(): void
+    {
+        $this->projects = Auth::user()->projects()->orderBy('created_at', 'desc')->get();
+    }
 
-//    public function refreshContactList(): void
-//    {
-//        $this->contacts = Auth::user()->contacts()->orderBy('created_at', 'desc')->get();
-//    }
+    public function refreshContactList(): void
+    {
+        $this->contacts = Auth::user()->contacts()->orderBy('created_at', 'desc')->get();
+    }
 
-//    public function selectProject($projectId): void
-//    {
-//        $selectedProject = Project::findOrFail($projectId);
-//
-//        $this->removeProjectFromCollection($this->projects, $selectedProject);
-//        $this->selectedProjects->push($selectedProject);
-//    }
+    public function selectProject($projectId): void
+    {
+        $selectedProject = Project::findOrFail($projectId);
 
-//    public function removeProject($projectId): void
-//    {
-//        $selectedProject = Project::find($projectId);
-//
-//        $this->removeProjectFromCollection($this->selectedProjects, $selectedProject);
-//        $this->projects->push($selectedProject);
-//    }
+        $this->removeProjectFromCollection($this->projects, $selectedProject);
+        $this->selectedProjects->push($selectedProject);
+    }
 
-//    protected function removeProjectFromCollection(&$collection, $selectedProject): void
-//    {
-//        $collection = $collection->filter(function ($project) use ($selectedProject) {
-//            return $project->id !== $selectedProject->id;
-//        });
-//    }
+    public function removeProject($projectId): void
+    {
+        $selectedProject = Project::find($projectId);
 
-//    public function removecontact($contactId): void
-//    {
-//        $this->selectedContacts = array_diff($this->selectedContacts, [$contactId]);
-//    }
+        $this->removeProjectFromCollection($this->selectedProjects, $selectedProject);
+        $this->projects->push($selectedProject);
+    }
 
-//    public function deleteJiri($jiriId): void
-//    {
-//        $jiri = Jiri::findOrFail($jiriId);
-//
-//        foreach ($jiri->jiriProjects as $jiriProject) {
-//            $jiriProject->contactDuties()->delete();
-//        }
-//
-//        $jiri->contactJiris()->delete();
-//        $jiri->jiriProjects()->delete();
-//        $jiri->delete();
-//
-//        $this->refreshJiriList();
-//    }
+    protected function removeProjectFromCollection(&$collection, $selectedProject): void
+    {
+        $collection = $collection->filter(function ($project) use ($selectedProject) {
+            return $project->id !== $selectedProject->id;
+        });
+    }
+
+    public function removecontact($contactId): void
+    {
+        $this->selectedContacts = array_diff($this->selectedContacts, [$contactId]);
+    }
+
+    public function deleteJiri($jiriId): void
+    {
+        $jiri = Jiri::findOrFail($jiriId);
+
+        foreach ($jiri->jiriProjects as $jiriProject) {
+            $jiriProject->contactDuties()->delete();
+        }
+
+        $jiri->contactJiris()->delete();
+        $jiri->jiriProjects()->delete();
+        $jiri->delete();
+
+        $this->refreshJiriList();
+    }
 }
