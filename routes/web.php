549a8f1f630027694b259186f1ac6e11a67bf95f
justinvincent <justin.vincent@student.hepl.be>
2024-08-07 10:10:52 +0200

    changed the redirect route when session is over

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::view('/', 'welcome')->name('welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
