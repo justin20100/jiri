22c3441bd54b0c22babcc613f6ca19f1de88b0c9
justinvincent <justin.vincent@student.hepl.be>
2024-08-08 12:55:58 +0200

    fixing errors style for forms

--- resources/views/livewire/welcome/contact-us.blade.php
+++ resources/views/livewire/welcome/contact-us.blade.php
@@ -4,14 +4,18 @@
             <label for="firstname" class="welcome-form__inputContainer__label">Prénom</label>
             <input wire:model="form.firstname" type="text" id="firstname" class="welcome-form__inputContainer__input" placeholder="Votre prénom">
             @error('form.firstname')
-                <span class="error">{{ $message }}</span>
+                <div class="errorContainer">
+                    <span class="errorContainer__error">{{ $message }}</span>
+                </div>
             @enderror
         </div>
         <div class="welcome-form__inputContainer">
             <label for="name" class="welcome-form__inputContainer__label">Nom</label>
             <input wire:model="form.lastname" type="text" id="name" class="welcome-form__inputContainer__input" placeholder="Votre nom">
             @error('form.lastname')
-                <span class="error">{{ $message }}</span>
+                <div class="errorContainer">
+                    <span class="errorContainer__error">{{ $message }}</span>
+                </div>
             @enderror
         </div>
     </div>
@@ -19,14 +23,18 @@
         <label for="email" class="welcome-form__inputContainer__label">Email</label>
         <input wire:model="form.email" type="email" id="email" class="welcome-form__inputContainer__input" placeholder="Votre email">
         @error('form.email')
-            <span class="error">{{ $message }}</span>
+            <div class="errorContainer">
+                <span class="errorContainer__error">{{ $message }}</span>
+            </div>
         @enderror
     </div>
     <div class="welcome-form__inputContainer">
         <label for="message" class="welcome-form__inputContainer__label">Message</label>
         <textarea wire:model="form.message" name="message" id="message" class="welcome-form__inputContainer__textarea" placeholder="Votre message"></textarea>
         @error('form.message')
-            <span class="error">{{ $message }}</span>
+            <div class="errorContainer">
+                <span class="errorContainer__error">{{ $message }}</span>
+            </div>
         @enderror
     </div>
     <button type="submit" class="welcome-form__button button">Envoyer</button>
