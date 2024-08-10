2d2a2611ff3e3f235e967a6807b35557b7c215c0
justinvincent <justin.vincent@student.hepl.be>
2024-08-10 10:47:38 +0200

    styling ConfirmModal

--- resources/views/livewire/tools/confirm-modal.blade.php
+++ resources/views/livewire/tools/confirm-modal.blade.php
@@ -1,10 +1,12 @@
-<div class="confirmModal">
+<div>
     @if($show)
-        <div class="confirmModal__contentContainer">
-            <p class="confirmModal__contentContainer__message">{{$message}}</p>
-            <div class="confirmModal__contentContainer__buttons">
-                <button wire:click="confirm" class="confirmModal__contentContainer__buttons__button confirmModal__contentContainer__buttons__button--confirm">Confirmer</button>
-                <button wire:click="cancel" class="confirmModal__contentContainer__buttons__button confirmModal__contentContainer__buttons__button--cancel">Annuler</button>
+        <div class="confirmModal">
+            <div class="confirmModal__contentContainer">
+                <p class="confirmModal__contentContainer__message">{{$message}}</p>
+                <div class="confirmModal__contentContainer__buttons">
+                    <button wire:click="cancel" class="confirmModal__contentContainer__buttons__button confirmModal__contentContainer__buttons__button--cancel button button--red">Annuler</button>
+                    <button wire:click="confirm" class="confirmModal__contentContainer__buttons__button confirmModal__contentContainer__buttons__button--confirm button">Confirmer</button>
+                </div>
             </div>
         </div>
     @endif
